679803ba88a748f27bb5145b96084073a7e6a244
Josh Wien <jwien001@gmail.com>
2013-11-17 16:03:02 -0800

    Initial random moving robot

---
+++ athena.py
@@ -0,0 +1,8 @@
+import rg
+
+from random import choice
+
+class Robot:
+	def act(self, game):
+		locs = rg.locs_around(self.location, filter_out=('invalid', 'obstacle'))
+		return ['move', choice(locs)]
