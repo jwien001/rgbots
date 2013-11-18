699c4ca4880658f97b38dbda24f08b726d9d12eb
Josh Wien <jwien001@gmail.com>
2013-11-17 17:09:19 -0800

    PEP8 compliance

--- athena.py
+++ athena.py
@@ -2,7 +2,8 @@ import rg
 
 from random import choice
 
+
 class Robot:
-	def act(self, game):
-		locs = rg.locs_around(self.location, filter_out=('invalid', 'obstacle'))
-		return ['move', choice(locs)]
+    def act(self, game):
+        locs = rg.locs_around(self.location, filter_out=('invalid', 'obstacle'))
+        return ['move', choice(locs)]
